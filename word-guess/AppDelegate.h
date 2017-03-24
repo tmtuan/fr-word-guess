2defcb31ed94dad5d58f69df00c28dc9b27d5e8a
mtuan <tranminhtuan684@gmail.com>
2017-03-24 09:11:31 +0700

    Initial Word Guessing Game

---
+++ word-guess/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  word-guess
+//
+//  Created by ThaoLT on 3/24/17.
+//  Copyright © 2017 tmtuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ word-guess/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  word-guess
+//
+//  Created by ThaoLT on 3/24/17.
+//  Copyright © 2017 tmtuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
